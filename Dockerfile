4010631924ee4373ddd001e5cdc3b794f2858072
Oleksii Pokhyl <alekcukp@gmail.com>
2023-11-07 04:27:45 +0200

    Added db dump, docker-compose adjustments

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM php:8.0-apache
 
 RUN apt-get update && \
     apt-get install -y \
+        mariadb-client \
         libzip-dev \
         libpng-dev \
         libpq-dev \
---
+++ mariadb/dump.sql
@@ -0,0 +1,89 @@
+-- MariaDB dump 10.19-11.1.2-MariaDB, for debian-linux-gnu (aarch64)
+--
+-- Host: localhost    Database: minimize-db
+-- ------------------------------------------------------
+-- Server version       11.1.2-MariaDB-1:11.1.2+maria~ubu2204
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `url_map`
+--
+
+DROP TABLE IF EXISTS `url_map`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `url_map` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url_key` varchar(6) NOT NULL,
+  `original_url` varchar(255) NOT NULL,
+  `redirects` int(10) unsigned NOT NULL DEFAULT 0,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url_key_index` (`url_key`) USING BTREE,
+  KEY `original_url_index` (`original_url`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `url_map`
+--
+
+LOCK TABLES `url_map` WRITE;
+/*!40000 ALTER TABLE `url_map` DISABLE KEYS */;
+/*!40000 ALTER TABLE `url_map` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`user`@`%`*/ /*!50003 TRIGGER set_url_map_values
+BEFORE INSERT ON url_map
+FOR EACH ROW
+BEGIN
+    DECLARE characters CHAR(62);
+    DECLARE urlKey VARCHAR(6);
+    DECLARE i INT;
+
+    SET characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    SET urlKey = '';
+
+    WHILE LENGTH(urlKey) < 6 DO
+        SET i = FLOOR(1 + (RAND() * 62));
+        SET urlKey = CONCAT(urlKey, SUBSTRING(characters, i, 1));
+    END WHILE;
+
+    SET NEW.expires_at = IFNULL(NEW.expires_at, NOW() + INTERVAL 3 DAY);
+    SET NEW.url_key = urlKey;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-07  1:55:31
